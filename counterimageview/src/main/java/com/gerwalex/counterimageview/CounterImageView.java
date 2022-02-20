484d17b03b40d33f08b8eb8148795329c36012e8
gerwalex <gerwalex@gmail.com>
2022-02-20 09:10:51 +0100

    Initial commit

---
+++ counterimageview/src/main/java/com/gerwalex/counterimageview/CounterImageView.java
@@ -0,0 +1,21 @@
+package com.gerwalex.counterimageview;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+public class CounterImageView extends View {
+    public CounterImageView(Context context) {
+        super(context);
+    }
+
+    public CounterImageView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CounterImageView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
