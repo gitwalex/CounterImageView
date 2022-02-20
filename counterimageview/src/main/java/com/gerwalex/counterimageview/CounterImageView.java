ed033ea2caad83af2221ddfe071b6f173b78120a
gerwalex <gerwalex@gmail.com>
2022-02-20 14:39:03 +0100

    First Release

--- counterimageview/src/main/java/com/gerwalex/counterimageview/CounterImageView.java
+++ counterimageview/src/main/java/com/gerwalex/counterimageview/CounterImageView.java
@@ -1,21 +1,86 @@
 package com.gerwalex.counterimageview;
 
 import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
-import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
 
 import androidx.annotation.Nullable;
+import androidx.core.content.res.ResourcesCompat;
+import androidx.databinding.BindingAdapter;
 
-public class CounterImageView extends View {
+import com.hookedonplay.decoviewlib.DecoView;
+import com.squareup.picasso.Picasso;
+import com.squareup.picasso.RequestCreator;
+
+import java.io.File;
+
+import jp.wasabeef.picasso.transformations.CropCircleTransformation;
+
+public class CounterImageView extends FrameLayout {
+    private final DecoView decoView;
+    private String filename;
+
+    @BindingAdapter(value = {"file"})
+    public static void setValue(CounterImageView view, String filename) {
+        view.setValue(filename);
+    }
+
     public CounterImageView(Context context) {
-        super(context);
+        this(context, null);
     }
 
     public CounterImageView(Context context, @Nullable AttributeSet attrs) {
-        super(context, attrs);
+        this(context, attrs, 0);
     }
 
     public CounterImageView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        setWillNotDraw(false);
+        ImageView imageView = new ImageView(context, attrs);
+        imageView.setPadding(30, 30, 30, 30);
+        decoView = new DecoView(context, attrs);
+        RequestCreator request;
+        if (isInEditMode()) {
+            imageView.setImageDrawable(ResourcesCompat.getDrawable(getResources(), R.drawable.img, context.getTheme()));
+        } else {
+            TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.CounterImageView);
+            try {
+                int file = a.getResourceId(R.styleable.CounterImageView_filename, -1);
+                if (file != -1) {
+                    request = Picasso.get().load(file);
+                } else {
+                    filename = a.getString(R.styleable.CounterImageView_filename);
+                    request = Picasso.get().load(new File(filename));
+                }
+                Drawable placeholder = a.getDrawable(R.styleable.CounterImageView_placeholder);
+                if (placeholder != null) {
+                    request.placeholder(placeholder);
+                }
+                int onError = a.getResourceId(R.styleable.CounterImageView_onError, -1);
+                if (onError != -1) {
+                    request.error(onError);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+                throw e;
+            } finally {
+                a.recycle();
+            }
+            request.transform(new CropCircleTransformation());
+            request.into(imageView);
+        }
+        addView(imageView);
+        addView(decoView);
+    }
+
+    public DecoView getDecoView() {
+        return decoView;
+    }
+
+    private void setValue(String filename) {
+        this.filename = filename;
     }
 }
--- test/src/main/java/com/gerwalex/counterimageview/FirstFragment.java
+++ test/src/main/java/com/gerwalex/counterimageview/FirstFragment.java
@@ -1,20 +1,26 @@
 package com.gerwalex.counterimageview;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.gerwalex.counterimageview.databinding.FragmentFirstBinding;
+import com.hookedonplay.decoviewlib.DecoView;
+import com.hookedonplay.decoviewlib.charts.SeriesItem;
+import com.hookedonplay.decoviewlib.events.DecoEvent;
 
 public class FirstFragment extends Fragment {
 
     private FragmentFirstBinding binding;
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         binding = FragmentFirstBinding.inflate(inflater, container, false);
         return binding.getRoot();
     }
@@ -23,5 +29,18 @@ public class FirstFragment extends Fragment {
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        DecoView decoView = binding.counterimage.getDecoView();
+        SeriesItem seriesItem = new SeriesItem.Builder(Color.parseColor("#FFE2E2E2")).setRange(0, 50, 0).build();
+        int backIndex = decoView.addSeries(seriesItem);
+        seriesItem = new SeriesItem.Builder(Color.parseColor("#FFFF8800")).setRange(0, 50, 0).build();
+        int series1Index = decoView.addSeries(seriesItem);
+        decoView.addEvent(new DecoEvent.Builder(50).setIndex(backIndex).build());
+        decoView.addEvent(new DecoEvent.Builder(16.3f).setIndex(series1Index).setDelay(5000).build());
+        decoView.addEvent(new DecoEvent.Builder(30f).setIndex(series1Index).setDelay(10000).build());
     }
 }
